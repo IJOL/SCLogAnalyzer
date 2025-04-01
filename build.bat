3bfb0ccb903e28c8acb017e4355c61056c36b25c
IJOL <nacho@apache.org>
2025-04-01 19:40:19 +0200

    Refactor build script to streamline version tagging and disable executable builds

--- build.bat
+++ build.bat
@@ -13,12 +13,6 @@ echo Incrementing version...
 python src\increment_version.py %commit_hash% %1
 echo.
 echo Building SC Log Analyzer...
-FOR /F "tokens=* USEBACKQ" %%F IN (`python -c "from src.version import get_version; print(get_version())"`) DO (
-  SET version=%%F
-)
-echo Current version: %version%
-
-
 :: Only push if the parameter was provided
 IF %PUSH_CHANGES%==1 (
   echo Committing and pushing version %version% to repository...
@@ -29,6 +23,20 @@ IF %PUSH_CHANGES%==1 (
 ) ELSE (
   echo Skipping push to remote repository. Use -p or --push parameter to push changes.
 )
+FOR /F "tokens=* USEBACKQ" %%F IN (`python -c "from src.version import get_version; print(get_version())"`) DO (
+  SET version=%%F
+)
+echo Current version: %version%
+if "%version%"=="" (
+  echo Version not found. Exiting.
+  exit /b 1
+)
+if "%1"=="" (
+  set version=test-%version%
+)
+echo Creating and pushing tag: %version%
+git tag -a %version% -m "Version %version%"
+git push origin %version%
 echo.
 echo Building Star Citizen Discord Logger Executable
 
@@ -40,15 +48,15 @@ call venv\Scripts\activate
 :: pip install -r requirements.txt
 
 :: Build executable using PyInstaller
-pyinstaller --onefile --console --clean --add-data "src/config.json.template;." --name log_analyzer src/log_analyzer.py
-pyinstaller --onefile --console --clean --add-data "src/bot/config.json.template;." --name StatusBoardBot src/bot/bot.py
+rem pyinstaller --onefile --console --clean --add-data "src/config.json.template;." --name log_analyzer src/log_analyzer.py
+rem pyinstaller --onefile --console --clean --add-data "src/bot/config.json.template;." --name StatusBoardBot src/bot/bot.py
 
 :: Build executable for SCLogAnalyzer GUI app
-pyinstaller --onefile --windowed --clean --add-data "src/config.json.template;." --add-binary "venv\Lib\site-packages\pyzbar\libiconv.dll;." --add-binary "venv\Lib\site-packages\pyzbar\libzbar-64.dll;." --name SCLogAnalyzer src/gui.py
+rem pyinstaller --onefile --windowed --clean --add-data "src/config.json.template;." --add-binary "venv\Lib\site-packages\pyzbar\libiconv.dll;." --add-binary "venv\Lib\site-packages\pyzbar\libzbar-64.dll;." --name SCLogAnalyzer src/gui.py
 
 :: Update ZIP files with new executables
-powershell -Command "Compress-Archive -Path 'dist\log_analyzer.exe', 'dist\readme' -DestinationPath 'dist\log_analyzer.zip' -Update"
-powershell -Command "Compress-Archive -Path 'dist\StatusBoardBot.exe' -DestinationPath 'dist\StatusBoardBot.zip' -Update"
-powershell -Command "Compress-Archive -Path 'dist\SCLogAnalyzer.exe', 'dist\readme' -DestinationPath 'dist\SCLogAnalyzer.zip' -Update"
+rem powershell -Command "Compress-Archive -Path 'dist\log_analyzer.exe', 'dist\readme' -DestinationPath 'dist\log_analyzer.zip' -Update"
+rem powershell -Command "Compress-Archive -Path 'dist\StatusBoardBot.exe' -DestinationPath 'dist\StatusBoardBot.zip' -Update"
+rem powershell -Command "Compress-Archive -Path 'dist\SCLogAnalyzer.exe', 'dist\readme' -DestinationPath 'dist\SCLogAnalyzer.zip' -Update"
 
 echo Build complete. Executable will be in the 'dist' directory.
