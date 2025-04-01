727ae1e7e36e4e6b80bf23f884d4dc55852d58a2
IJOL <nacho@apache.org>
2025-04-01 15:01:49 +0200

    Increment version to

--- src/version.py
+++ src/version.py
@@ -1,11 +1,11 @@
 """Version information for SC Log Analyzer"""
 
-MAJOR = 1
-MINOR = 0
-RELEASE = 0
-PATCH = "0e9cd20"
+MAJOR = 0
+MINOR = 1
+RELEASE = 1
+PATCH = "4ce3949"
 
-VERSION = f"{MAJOR}.{MINOR}.{RELEASE} - {PATCH}"
+VERSION = f"{MAJOR}.{MINOR}.{RELEASE}-{PATCH}"
 
 def get_version():
     """Return the full version string"""
